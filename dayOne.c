a3b5d1a31b2b2979f4410543172cc7043641fc19
Tim Wenzel <48419301+timwenzeltech@users.noreply.github.com>
2023-12-06 12:25:55 +0100

    not great not terrible

--- dayOne.c
+++ dayOne.c
@@ -1,56 +1,80 @@
-/*
-Day 1: Trebuchet?!
-The newly-improved calibration document consists of lines of text; each str originally contained a specific calibration value that the
-Elves now need to recover. On each str, the calibration value can be found by combining the first digit and the last digit
-(in that order) to form a single two-digit number.
-
-For example:
-
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
-In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
-
-Consider your entire calibration document. What is the sum of all of the calibration values?
-*/
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
-int main(){
-    const int MAX_LEN = 100;
-    FILE *file;
-    char str[MAX_LEN];
-    int sum = 0;
+int checkForIntStrings(char *line){
+    char *numbers[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    int values[] = {1,2,3,4,5,6,7,8,9};
 
+    for(int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++){
+        if(strcmp(line, numbers[i]) == 0){
+            return values[i];
+        }
+    }
+    return -1;
+}
+
+int getFirstInt(char *str){
+    int firstInt = -1;
+    char substring[50];
+    int index = 0;
+
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isdigit(str[i])){
+            firstInt = str[i] - '0';
+            break; //Terminate so firstInt doesnt get overwritten
+        }else{
+            
+        }
+    }
+    return firstInt;
+}
+
+int getLastInt(char *str){
+    int lastInt = -1;
+    char substring[50];
+    int index = 0;
+    for(int i = 0; str[i] != '\0'; i++){
+        if(isdigit(str[i])){
+            lastInt = str[i] - '0';
+        }else{
+            
+        }
+    }
+    return lastInt;
+}
+
+int main(){
+    //Declares Variables for file, str(which will represent a line in the file) and sum(sum of all values)
+    FILE *file;
+    char str[100]; // String Array, which will represent the strings of every line
+    int sum = 0; // Sum of first int*10 and last Int 
+
+    //Opens File, checks for null
     file = fopen("input.txt", "r");
     if(file==NULL){
         perror("Can't open file");
         return 1;
     }
-
+    //Iterates line by line through file:
     while (fgets(str, sizeof(str), file) != NULL) {
-        int firstInt = 0, lastInt = 0;
-        int count = -1;
 
-        for(int i = 0; str[i] != '\0'; i++){
-            if(isdigit(str[i])){
-                count++;
-                if(count == 0){
-                    firstInt = str[i] - '0';
-                }else if(count > 0){
-                    lastInt = str[i] - '0';
-                }
-            }
+        int firstInt = getFirstInt(str); 
+        int lastInt = getLastInt(str);
+
+        if(firstInt != -1){
+            printf("First Number Found is: %d" , firstInt);
+        }else{
+            firstInt = 0;
         }
-
-        if(lastInt == 0){lastInt = firstInt;}
-        printf("firstInt: %d. lastInt: %d\n", firstInt, lastInt);
+        if(lastInt != -1){
+            printf(" last Number Found is: %d\n", lastInt);
+        }else{
+            lastInt = 0;
+        }
         sum = sum + firstInt*10 + lastInt;
     }
     printf("Sum of all the calibration values is: %d\n", sum);
     fclose(file);
     return 0;
 }
-
